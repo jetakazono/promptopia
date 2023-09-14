739a4b0b9e9558066b85a5999781dd80bf22fb9e
jetakazono <jessica.takazono@gmail.com>
2023-09-15 00:59:45 +0100

    promptopia project

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,24 @@
+import { connectToDB } from "@utils/database";
+import NextAuth from "next-auth/next";
+import GoogleProvider from 'next-auth/providers/google'
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    async session({ session }) {
+
+    },
+    async signIn({  profile }) {
+        try {
+            await connectToDB()
+        } catch (error) {
+            
+        }
+    }
+})
+
+export { handler as GET, handler as POST }
