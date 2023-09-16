2e5db458dca297726b0d42d94407eeef925e4831
jetakazono <jessica.takazono@gmail.com>
2023-09-16 23:12:52 +0100

    add profile

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,17 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, params) => {
+    try {
+        await connectToDB()    
+        
+        const prompt = await Prompt.findById(params.id).populate('creator')
+
+        if(!prompt) return new Response('Prompt not find', { status: 404 })
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+
+    } catch (error) {
+        return new Response('Failed to fetch prompt', { status: 500 })
+    }
+}
