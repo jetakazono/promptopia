f1ab6f65fe8d2b6ef1bd5acaae6f0f37c390c378
jetakazono <jessica.takazono@gmail.com>
2023-09-17 02:24:30 +0100

    add edit and delete

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,7 +1,7 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
-export const GET = async (request, params) => {
+export const GET = async (request, {params}) => {
     try {
         await connectToDB()    
         
@@ -13,5 +13,38 @@ export const GET = async (request, params) => {
 
     } catch (error) {
         return new Response('Failed to fetch prompt', { status: 500 })
+    }
+}
+
+export const PATCH = async ( request, {params}) => {
+    const { prompt, tag } = await request.json();
+    
+    try {
+        await connectToDB()  
+
+        const existingPrompt = await Prompt.findById(params.id)
+
+        if(!existingPrompt) return new Response("Prompt not find", { status: 404})
+
+        existingPrompt.prompt = prompt
+        existingPrompt.tag = tag
+
+        await existingPrompt.save()
+
+        return new Response("Successfully updated the Prompts", { status: 200 })
+
+    } catch (error) {
+        return new Response("Failed to update prompt", { status: 500 })
+    }
+}
+
+export const DELETE = async (request, {params}) => {
+    try {
+        await connectToDB()
+
+        await Prompt.findByIdAndRemove(params.id) 
+        return new Response('Prompt deleted successfully', {status: 200})
+    } catch (error) {
+        return new Response("Failed to delete prompt", { status: 500 })
     }
 }
